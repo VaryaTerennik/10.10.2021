44c82c2ac0f294c43f9dd814f2506fb3b21688ea
VaryaTerennik <varvara.terennik@gmail.com>
2021-12-01 16:22:33 +0300

    main

--- src/components/AuthButton.js
+++ src/components/AuthButton.js
@@ -2,7 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Button, Box } from "@mui/material";
 
 function AuthButton() {
-  const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
   return (
     <>
       {!isAuthenticated && (
--- src/redux/MessagesSlice.js
+++ src/redux/MessagesSlice.js
@@ -1,98 +1,90 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
- chats: {}
-}
+  chats: {},
+};
 
 export const fetchMessages = createAsyncThunk(
-  'users/fetchMessages',
+  "users/fetchMessages",
   async (chatId) => {
-    const response = await fetch(`https://inordic-messenger-api.herokuapp.com/chats/${chatId}/messages`)
-    const messages = await response.json()
+    const response = await fetch(
+      `https://inordic-messenger-api.herokuapp.com/chats/${chatId}/messages`
+    );
+    const messages = await response.json();
     return {
       chatId,
-      messages: messages
-     }
+      messages: messages,
+    };
   }
-)
+);
 
 export const deleteMessage = createAsyncThunk(
-  'users/deleteMessage',
+  "users/deleteMessage",
   async (data) => {
-    const chatId = data.chatId
-    const messageId = data.messageId
-    const response = await fetch(`https://inordic-messenger-api.herokuapp.com/chats/${chatId}/messages/${messageId}`, {
-      method: "DELETE", 
-      body: JSON.stringify({data}),
-      headers: {
-        'Content-Type': 'application/json'
+    const chatId = data.chatId;
+    const messageId = data.messageId;
+    const response = await fetch(
+      `https://inordic-messenger-api.herokuapp.com/chats/${chatId}/messages/${messageId}`,
+      {
+        method: "DELETE",
+        body: JSON.stringify({ data }),
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
-    })
-    const messages = await response.json()
+    );
+    const messages = await response.json();
     return {
       chatId,
-      messageId
-     }
+      messageId,
+    };
   }
-)
+);
 
-
-export const  MessagesSlice = createSlice({
-  name: 'messageslist',
+export const MessagesSlice = createSlice({
+  name: "messageslist",
   initialState,
   reducers: {
     addMessage: (state, action) => {
-        const {chatId} = action.payload
-        const messages = state.chats[chatId]?.messages || []
-        const newMessage = action.payload
-        if(!state.chats[chatId]) {
-            state.chats[chatId] = {
-                messages: [newMessage]
-            }
-        } else {
-        state.chats[chatId].messages = [...messages, newMessage]
-        }
-      },  
-      postMessage: () => {
-
-      },
-
-      // deleteMessage: (state, action) => {
-      //   const {chatId} = action.payload
-      //   const messages = state.chats[chatId]?.messages || []
-      //   const delMessage = action.payload
-      //   if(!state.chats[chatId]) {
-      //       state.chats[chatId] = {
-      //           messages: [newMessage]
-      //       }
-      //   } else {
-      //   state.chats[chatId].messages = [...messages, newMessage]
-      //   }
-      // },  
+      const { chatId } = action.payload;
+      const messages = state.chats[chatId]?.messages || [];
+      const newMessage = action.payload;
+      if (!state.chats[chatId]) {
+        state.chats[chatId] = {
+          messages: [newMessage],
+        };
+      } else {
+        state.chats[chatId].messages = [...messages, newMessage];
+      }
+    },
+    postMessage: () => {},
   },
 
   extraReducers: (builder) => {
     builder.addCase(fetchMessages.fulfilled, (state, action) => {
-      const {chatId, messages} = action.payload
-      if(!state.chats[chatId]) {
+      const { chatId, messages } = action.payload;
+      if (!state.chats[chatId]) {
         state.chats[chatId] = {
-            messages
-        }
-    } else {
-    state.chats[chatId].messages = messages
-    }
-    })
+          messages,
+        };
+      } else {
+        state.chats[chatId].messages = messages;
+      }
+    });
 
     builder.addCase(deleteMessage.fulfilled, (state, action) => {
-      const {chatId, messageId} = action.payload
-      state.chats[chatId].messages.splice((state.chats[chatId].messages.findIndex(el => el._id === messageId)), 1)
-    })
+      const { chatId, messageId } = action.payload;
+      state.chats[chatId].messages.splice(
+        state.chats[chatId].messages.findIndex((el) => el._id === messageId),
+        1
+      );
+    });
   },
+});
 
-})
+export const { addMessage, postMessage } = MessagesSlice.actions;
 
-export const { addMessage, postMessage } = MessagesSlice.actions
+export const messageChatSelector = (chatId) => (state) =>
+  state.messageslist.chats[chatId]?.messages || [];
 
-export const messageChatSelector = chatId => state => (state.messageslist.chats[chatId]?.messages || [])
-
-export default MessagesSlice.reducer
+export default MessagesSlice.reducer;
